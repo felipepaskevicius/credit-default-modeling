bd1ebe680c643fe9db31b900581badd7bfcb1005
Felipe Paskevicius <xmourax@hotmail.com>
2024-11-15 18:02:23 -0300

    mais um

--- scripts/train.py
+++ scripts/train.py
@@ -1,13 +1,7 @@
-from sklearn import model_selection
+from sklearn.model_selection import train_test_split
 
 # method para dividir os dados em treino e teste
-X_Train, X_Test, y_train, y_test = model_selection.train_test_split(
-                                                                    X, 
-                                                                    y,
-                                                                    test_size=0.2, 
-                                                                    random_state=42, 
-                                                                    stratify=y
-)
+X_Train, X_Test, y_train, y_test = train_test_split(X, y, test_size=0.2,  random_state=42,  stratify=y)
 
 # Check the dimensions of the training and testing datasets
 print(X_Train.shape, X_Test.shape, y_train.shape, y_test.shape)
